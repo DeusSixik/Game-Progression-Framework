9288208d02b4604c30ae735c44b3756ffd27890e
Bogdan <142807122+DeusSixik@users.noreply.github.com>
2026-05-31 00:15:47 +0300

    fix: Crash on reload

--- src/main/java/dev/sixik/gpf/impl/mixin/MixinMinecraftServer.java
+++ src/main/java/dev/sixik/gpf/impl/mixin/MixinMinecraftServer.java
@@ -4,17 +4,17 @@ import dev.sixik.gpf.registry.StagesRegistry;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.function.Consumer;
 
 @Mixin(MinecraftServer.class)
 public class MixinMinecraftServer {
 
-    @Redirect(method = "reloadResources", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/CompletableFuture;thenAcceptAsync(Ljava/util/function/Consumer;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"))
-    public <T> CompletableFuture<Void> gpf$reloadResources(CompletableFuture instance, Consumer<? super T> action, Executor executor) {
-        return instance.thenAcceptAsync((v) ->  StagesRegistry.INSTANCE.reloadFromScripts(), executor).thenAcceptAsync(action, executor);
+    @Inject(method = "reloadResources", at = @At(value = "RETURN"))
+    public void gpf$reloadResources(Collection<String> selectedIds, CallbackInfoReturnable<CompletableFuture<Void>> cir) {
+        StagesRegistry.INSTANCE.reloadFromScripts();
     }
 }
